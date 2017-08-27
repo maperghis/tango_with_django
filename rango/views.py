6423bcd826156edd069184afe504cff41a235545
Miranda Aperghis <miranda.aperghis@gmail.com>
2017-08-27 11:49:15 +0800

    Index page renders the index.html page template

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!"
-        "<br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page."
